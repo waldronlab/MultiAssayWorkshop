63e4c2379c905d77640cabc291b57bb1d5a2c866
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-01-19 13:05:51 -0500

    install from GitHub

--- inst/scripts/install_deps.R
+++ inst/scripts/install_deps.R
@@ -1,8 +1,13 @@
-remotes::install_local(
-    dependencies = TRUE,
-    repos = BiocManager::repositories(),
-    build_vignettes = TRUE,
-    upgrade = TRUE
+BiocManager::install(update = TRUE, ask = FALSE)
+
+BiocManager::install(
+    "waldronlab/MultiAssayWorkshop", 
+    dependencies = TRUE, build_vignettes = TRUE
 )
 
-BiocManager::install(update = TRUE, ask = FALSE)
+# remotes::install_local(
+#     dependencies = TRUE,
+#     repos = BiocManager::repositories(),
+#     build_vignettes = TRUE,
+#     upgrade = TRUE
+# )
